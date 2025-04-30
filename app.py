760cfc007fb7549525f64a69b586a70b7d8da66c
iamprashant <iamprashantjain2601@gmail.com>
2025-04-30 23:25:16 +0530

    linting issues fixed

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-import math, sys
+def calc(x, y, op):
+    """Performs basic arithmetic operations based on the operator."""
+    if op == '+':
+        return x + y
+    elif op == '-':
+        return x - y
+    elif op == '*':
+        return x * y
+    elif op == '/':
+        if y == 0:
+            print("Can't divide by zero")
+        else:
+            return x / y
+    else:
+        print("Invalid operator")
 
-def calc( x ,y,op ):
- if op =='+':
-    return x+y
- elif op=='-':
-        return x -y
- elif op=='*':
-  return x*y
- elif op=='/':
-       if y==0:
-        print("Can't divide by zero")
-       else:
-        return x/y
- else:
-     print( "Invalid op" )
 
-a= 5
-b= 1
-operator = "+"
-result = calc(a,b,operator)
-print(  "The result is:",result)
+def main():
+    a = 5
+    b = 1
+    operator = "+"
+    result = calc(a, b, operator)
+    print("The result is:", result)
+
+
+if __name__ == "__main__":
+    main()
